5c471135e4eb54f9b4d489a8c45624a8186a7367
jeanine_c <55642941+jeaninening@users.noreply.github.com>
2026-03-30 18:58:13 +0800

    Add WSGI handler for app
    Added WSGI handler for deployment requirements.

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
+    
 app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
+
+# 必须加这个入口（阿里云要求）
+def handler(environ, start_response):
+    return app(environ, start_response)
